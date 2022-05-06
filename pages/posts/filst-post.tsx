056831cb66d646cf26dacc0aec486e114e65943c
kida-works <kida.works.1234@gmail.com>
2022-05-06 23:03:04 +0900

    End of course

---
+++ pages/posts/filst-post.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+export default function FilstPost() {
+  return (
+    <>
+      <h1>Filst post</h1>
+      <Link href="/">
+        <a>Bacl to Home</a>
+      </Link>
+    </>
+  )
+}
