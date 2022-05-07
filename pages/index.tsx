8ba8550bdec4089f3b588d5f894e49471ea2aa7c
kida-works <kida.works.1234@gmail.com>
2022-05-08 00:10:36 +0900

    End of course

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,21 +2,59 @@ import Head from 'next/head';
 import Link from 'next/link';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../lib/posts';
 
-export default function Home() {
+// static Generation（基本こっち）
+// export async function getStaticProps() {
+//   const allPostsData = getSortedPostsData();
+//   return {
+//     props: {
+//       allPostsData,
+//     },
+//   };
+// }
+
+// server-side Rendering
+export async function getServerSideProps(context:any) {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home( allPostsData : any ) {
+  console.log(allPostsData)
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
+
       <section className={utilStyles.headingMd}>
         <p>There is no defeat in the void.</p>
-        <p>空白に敗北はない。</p>
+        <p>空白に敗北はない。お風呂は嫌い！</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
-        <Link href="/posts/filst-post">filst-post</Link>
+        <Link href="/posts/first-post">first-post</Link>
+      </section>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.allPostsData.map(({ id, date, title }: any) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
